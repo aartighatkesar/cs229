533a72057ea1f6a9c30e834d82b7b4f68144a091
Rohit Apte <rohit@rohitapte.com>
2018-10-15 15:52:49 +0800

    asdf

--- ps1/src/p01b_logreg.py
+++ ps1/src/p01b_logreg.py
@@ -18,16 +18,18 @@ def main(train_path, eval_path, pred_path):
     # Train a logistic regression classifier
     # Plot decision boundary on top of validation set set
     # Use np.savetxt to save predictions on eval set to pred_path
-    print(x_train.shape)
-    print(y_train.shape)
+    x_eval, y_eval= util.load_dataset(eval_path, add_intercept=True)
     logreg=LogisticRegression()
     logreg.fit(x_train,y_train)
-    #logreg.pre
+    print(logreg.theta)
+    output=logreg.predict(x_eval)
+    np.savetxt(pred_path,output)
     # *** END CODE HERE ***
 
 
 class LogisticRegression(LinearModel):
     """Logistic regression with Newton's Method as the solver."""
+
 
     def fit(self, x, y):
         """Run Newton's Method to minimize J(theta) for logistic regression.
@@ -37,17 +39,18 @@ class LogisticRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
-        theta=np.zeros((x.shape[1],1))
+        if self.theta is None:
+            self.theta=np.zeros((x.shape[1],1))
         y=y.reshape((y.shape[0],1))
         error=1e9
         numIters=0
-        while error>1e-5:
-            hess=util.hessian(x,theta)
-            Jprime=util.gradient(x,theta,y)
+        while error>1e-5 and numIters<self.max_iter:
+            hess=util.hessian(x,self.theta)
+            Jprime=util.gradient(x,self.theta,y)
             hessInv=np.linalg.inv(hess)
-            theta_new=theta-hessInv.dot(Jprime)
-            error=np.sum(np.abs(theta-theta_new))
-            theta=theta_new.copy()
+            theta_new=self.theta-hessInv.dot(Jprime)
+            error=np.sum(np.abs(self.theta-theta_new))
+            self.theta=theta_new.copy()
             numIters+=1
         # *** END CODE HERE ***
 
@@ -61,4 +64,5 @@ class LogisticRegression(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        return util.sigmoid(x.dot(self.theta))
         # *** END CODE HERE ***
