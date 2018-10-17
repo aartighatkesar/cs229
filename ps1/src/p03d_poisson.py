2306a030fcf37bfb1122e8b739e4ce6237e27b3b
Rohit Apte <rohit@rohitapte.com>
2018-10-17 12:58:25 +0800

    problem set 3 completed

--- ps1/src/p03d_poisson.py
+++ ps1/src/p03d_poisson.py
@@ -18,7 +18,13 @@ def main(lr, train_path, eval_path, pred_path):
 
     # *** START CODE HERE ***
     # Fit a Poisson Regression model
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=False)
+    poisonreg = PoissonRegression(step_size=lr)
+    poisonreg.fit(x_train, y_train)
+    print(poisonreg.theta)
     # Run on the validation set, and use np.savetxt to save outputs to pred_path
+    output = poisonreg.predict(x_eval)
+    np.savetxt(pred_path, output)
     # *** END CODE HERE ***
 
 
@@ -31,6 +37,28 @@ class PoissonRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+        mini_batch_size=100
+        if self.theta is None:
+            self.theta=np.zeros(x.shape[1])
+        error=1e9
+        numIters=0
+        while error > self.eps and numIters < self.max_iter:
+            for i in range(0,x.shape[0],mini_batch_size):
+                end_index=min(i+mini_batch_size,x.shape[0])
+                if end_index>x.shape[0]:
+                    end_index=x.shape[0]
+                x_batch=x[i:end_index,:]
+                theta_x=np.dot(x_batch,self.theta)
+                exp_theta_x=np.exp(theta_x)
+                diff=y[i:end_index]-exp_theta_x
+                prod=np.dot(x_batch.T,diff)
+                avg=prod/x_batch.shape[0]
+                theta_new=self.theta+self.step_size*avg
+                error = np.sum(np.abs(self.theta - theta_new))
+                if error < self.eps:
+                    break
+                self.theta=theta_new.copy()
+            numIters+=1
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -43,4 +71,6 @@ class PoissonRegression(LinearModel):
             Floating-point prediction for each input, shape (m,).
         """
         # *** START CODE HERE ***
+        theta_x=np.dot(x,self.theta)
+        return np.exp(theta_x)
         # *** END CODE HERE ***
